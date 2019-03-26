f523c2cf7cba8d7b2ac7a4b36daaf7d8bfc65e2d
María Alejandra Barajas <alepa12@hotmail.es>
2019-03-26 13:56:05 -0500

    Todo listo :)

---
+++ src/codigo/Marciano.java
@@ -0,0 +1,35 @@
+
+package codigo;
+
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Sharon
+ */
+public class Marciano {
+    public Image imagen1, imagen2 = null;
+    public int x = 0;
+    public int y = 0;
+    private int vX = 1;
+    public boolean vivo = true;
+    
+    public Marciano(){
+        
+    }
+    
+    public void mueve(){
+        x += vX;
+    }
+
+    public void setvX(int vX) {
+        this.vX = vX;
+    }
+
+    public int getvX() {
+        return vX;
+    }
+    
+}
